53a583e872a595ee81c24c3ddd5a023f8043d198
MJU <marie.ju2012@outlook.com>
2020-03-12 20:30:17 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+lert i = 0
+
+function doWhileLoop(num) {
+  do {
+    console.log(i, num)
+  } while (incrementVariable() < num)
+}
+
+function incrementVariable() {
+  return i += 1
+}
+
+function 
